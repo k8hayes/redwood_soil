a72dc6dd3c38eec485ff10a7afd591f916f47720
Kate <katherine.hayes@ucdenver.edu>
2021-01-19 09:47:36 -0700

    clean up (nothing in here i need)

--- code/calibrated.R
+++
@@ -1,8 +0,0 @@
-# plotting calibrated ages
-setwd('/Users/katherinehayes/Desktop/Thesis/Data/')
-calib <- read.csv('calout.csv')
-
-hist(calib$median.probability)
-
-  age_reversals <- read.csv('age_reversal.csv')
-age_reversals

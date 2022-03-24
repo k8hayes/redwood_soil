5065f2800c0e03716d56694e8f1a01e6db3285a7
Kate <katherine.hayes@ucdenver.edu>
2022-03-24 11:38:55 -0600

    taking out 30-35

--- code/FigS1_bulkdensity.R
+++ code/FigS1_bulkdensity.R
@@ -1,6 +1,6 @@
 # script for producing bulk density graph
 # appendix 1: figure s1
-library(ggplot2)
+library(tidyverse)
 library(here)
 library(cowplot)
 theme_set(theme_cowplot())
@@ -8,13 +8,13 @@ theme_set(theme_cowplot())
 char_mass <- read.csv(here("Data/char_mass_sieve.csv"))
 
 # calculating the volume of the soil rings used
-ring_volume <- pi * 2.5^2 * 5 # 98.17477
+ring_volume <- pi * 2.5^2 * 5 # 98.17477 cm
 
 # adding column to char_mass for bulk density
-char_mass$bulk_density <- char_mass$kmd_dry_weight/ring_volume 
+char_mass$bulk_den <- char_mass$kmd_dry_weight/ring_volume 
 
 # attaching order to depth
-char_mass$depth <- factor(depth, levels = c("0-5", "5-10", "10-15",
+char_mass$depth <- factor(char_mass$depth, levels = c("0-5", "5-10", "10-15",
                                             "15-20", "20-25", "25-30", "30-35"))
 # changing depth to cm
 char_mass$depth[char_mass$depth_category == "A"] <- "0-5"
@@ -24,6 +24,9 @@ char_mass$depth[char_mass$depth_category == "D"] <- "15-20"
 char_mass$depth[char_mass$depth_category == "E"] <- "20-25"
 char_mass$depth[char_mass$depth_category == "F"] <- "25-30"
 char_mass$depth[char_mass$depth_category == "G"] <- "30-35"
+
+char_mass <- char_mass %>%
+  filter(depth != "30-35")
 
 # exploring relationship between bulk density and depth
 ggplot(char_mass) + geom_boxplot(aes(depth, bulk_density), fill = "grey") + 
